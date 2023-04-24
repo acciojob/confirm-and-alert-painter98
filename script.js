a3fc02f525c30b49742a51c8481ccd05fb9e65f9
painter98 <120320713+painter98@users.noreply.github.com>
2023-04-24 17:47:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let sure = confirm("Do you want to proceed?");
+if(sure)
+	alert(“You clicked OK. Proceeding...”);
+else
+	alert( "You clicked Cancel. Exiting...");
